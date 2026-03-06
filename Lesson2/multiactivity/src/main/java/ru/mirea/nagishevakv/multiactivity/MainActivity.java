cd7d7f708cb95e93a749581eaf3939dabc72ca14
Ксения Нагишева <knagisheva@gmail.com>
2026-03-06 21:56:27 +0300

    Sending text between actions

--- Lesson2/multiactivity/src/main/java/ru/mirea/nagishevakv/multiactivity/MainActivity.java
+++ Lesson2/multiactivity/src/main/java/ru/mirea/nagishevakv/multiactivity/MainActivity.java
@@ -3,6 +3,7 @@ package ru.mirea.nagishevakv.multiactivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,8 +13,11 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText textForInput;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
@@ -26,6 +30,14 @@ public class MainActivity extends AppCompatActivity {
 
     public void onClickNewActivity(View view) {
         Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+    }
+
+    public void SendInputText(View view) {
+        textForInput = findViewById(R.id.tiForInput);
+        String text = String.valueOf(textForInput.getText());
+        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+        intent.putExtra("inputedText", text);
         startActivity(intent);
     }
 }
--- Lesson2/multiactivity/src/main/java/ru/mirea/nagishevakv/multiactivity/SecondActivity.java
+++ Lesson2/multiactivity/src/main/java/ru/mirea/nagishevakv/multiactivity/SecondActivity.java
@@ -7,6 +7,7 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -17,8 +18,8 @@ import ru.mirea.nagishevakv.multiactivity.databinding.ActivitySecondBinding;
 
 public class SecondActivity extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
     private ActivitySecondBinding binding;
+    private TextView textViewForInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,26 +28,10 @@ public class SecondActivity extends AppCompatActivity {
         binding = ActivitySecondBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        setSupportActionBar(binding.toolbar);
+        textViewForInput = findViewById(R.id.tvForInput);
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_second);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAnchorView(R.id.fab)
-                        .setAction("Action", null).show();
-            }
-        });
+        String text = (String) getIntent().getSerializableExtra("inputedText");
+        textViewForInput.setText(text);
     }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_second);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
-    }
 }
