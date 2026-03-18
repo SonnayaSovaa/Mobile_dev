46645537e271e1dc922d272ed6bdfaaf64ac41f5
Ксения Нагишева <knagisheva@gmail.com>
2026-03-18 23:43:16 +0300

    Activity lifecycle update

--- Lesson2/activitylifecycle/src/main/java/ru/mirea/nagishevakv/activitylifecycle/MainActivity.java
+++ Lesson2/activitylifecycle/src/main/java/ru/mirea/nagishevakv/activitylifecycle/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        Log.i(TAG, "-------onCreate()------");
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -28,6 +29,51 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        Log.i(TAG, "onStart()");
+        Log.i(TAG, "-------onStart()------");
+    }
+
+
+    @Override
+    protected void onRestoreInstanceState(Bundle saveInstanceState){
+        super.onRestoreInstanceState(saveInstanceState);
+        Log.i(TAG, "-------onRestoreInstanceState()------");
+    }
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        Log.i(TAG, "-------onRestart()------");
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.i(TAG, "-------onResume()------");
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.i(TAG, "-------onPause()------");
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle saveInstanceState){
+        super.onSaveInstanceState(saveInstanceState);
+        Log.i(TAG, "-------onSaveInstanceState()------");
+
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.i(TAG, "-------onStop()------");
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.i(TAG, "-------onDestroy()------");
     }
 }
