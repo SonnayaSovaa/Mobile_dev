42ebc988c731764ebe9f631e0097dedfd2077e10
Ксения Нагишева <knagisheva@gmail.com>
2026-04-14 17:33:31 +0300

    Text + buttons binding

--- Lesson4/app/src/main/java/ru/mirea/nagishevakv/lesson4/MainActivity.java
+++ Lesson4/app/src/main/java/ru/mirea/nagishevakv/lesson4/MainActivity.java
@@ -1,6 +1,8 @@
 package ru.mirea.nagishevakv.lesson4;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,21 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import ru.mirea.nagishevakv.lesson4.databinding.ActivityMainBinding;
 
+public	class	MainActivity	extends	AppCompatActivity	{
+    private ActivityMainBinding binding;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle	savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.editTextMirea.setText("Мой	номер	по	списку	№16");
+        binding.buttonMirea.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(MainActivity.class.getSimpleName(), "onClickListener");
+            }
         });
     }
 }
